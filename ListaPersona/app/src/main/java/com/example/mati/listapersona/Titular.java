c2b8f8ed54b09d40ea122926270898254a4ef5e7
DaniieR <dam.dromero@institutoserpis.org>
2016-10-24 13:16:58 +0200

    Acabado SpinnerObjeto con paso a segunda pantalla

---
+++ ListaPersona/app/src/main/java/com/example/mati/listapersona/Titular.java
@@ -0,0 +1,25 @@
+package com.example.mati.listapersona;
+
+/**
+ * Created by mati on 17/10/16.
+ */
+public class Titular
+{
+    private String titulo;
+    private String subtitulo;
+    private int imagen;
+    public Titular(String tit, String sub, int img){
+        titulo = tit;
+        subtitulo = sub;
+        imagen = img;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getSubtitulo(){
+        return subtitulo;
+    }
+    public int getImagen(){
+        return imagen;
+    }
+}
